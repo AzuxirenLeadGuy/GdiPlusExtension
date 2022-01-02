c3cf2ed6f4f0d03217f0f96b999f932af719901d
Ashish <sam4lordjesus@outlook.com>
2022-01-02 21:15:45 +0530

    Brightness feature

--- GdiPlusExtensions/src/Common.cs
+++ GdiPlusExtensions/src/Common.cs
@@ -1,5 +1,5 @@
 using System.Drawing;
-
+using System;
 namespace GdiPlusExtensions
 {
 	/// <summary>This static class contains extension functions for Bitmap</summary>
@@ -31,6 +31,41 @@ namespace GdiPlusExtensions
 			if (value <= min) return min;
 			else if (value >= max) return max;
 			else return value;
+		}
+		/// <summary>
+		/// Applies a function on every pixel of the image
+		/// </summary>
+		/// <param name="bitmap">The image to affect</param>
+		/// <param name="func">The function to apply</param>
+		public static void ApplyFuncForeachPixel(this Bitmap bitmap, Func<Color, Color> func)
+		{
+			int w = bitmap.Width, h = bitmap.Height, i, j;
+			for (i = 0; i < w; i++)
+			{
+				for (j = 0; j < h; j++)
+				{
+					bitmap.SetPixel(i, j, func(bitmap.GetPixel(i, j)));
+				}
+			}
+		}
+		/// <summary>
+		/// Applies brightness change to the image
+		/// </summary>
+		/// <param name="bitmap">The image for which the brightness is to be affected</param>
+		/// <param name="bf">The brightness factor</param>
+		public static void AdjustBrightness(this Bitmap bitmap, sbyte bf)
+		{
+			bitmap.ApplyFuncForeachPixel(adjust);
+			Color adjust(Color c)
+			{
+				return Color.FromArgb(Clamp(c.R + bf), Clamp(c.G + bf), Clamp(c.B + bf));
+			}
+		}
+		internal static byte Clamp(int x)
+		{
+			if (x <= 0) return 0;
+			else if (x >= 0xff) return 0xff;
+			else return (byte)x;
 		}
 	}
 }
